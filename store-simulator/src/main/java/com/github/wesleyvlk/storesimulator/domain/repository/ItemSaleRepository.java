22632bed84c28b037bcdcfeff1b83ae0535abcc0
wesleyvlk <wesleymuniz20@gmail.com>
2023-04-06 14:58:50 -0300

    feat: add repository user product sale itemsale

---
+++ store-simulator/src/main/java/com/github/wesleyvlk/storesimulator/domain/repository/ItemSaleRepository.java
@@ -0,0 +1,7 @@
+package com.github.wesleyvlk.storesimulator.domain.repository;
+
+import com.github.wesleyvlk.storesimulator.domain.model.ItemSale;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ItemSaleRepository extends CrudRepository<ItemSale, Integer> {
+}
