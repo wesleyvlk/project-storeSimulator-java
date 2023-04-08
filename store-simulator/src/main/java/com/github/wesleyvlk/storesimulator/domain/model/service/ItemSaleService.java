9f57273692991d763792c8038a3f99773c83fc6d
wesleyvlk <wesleymuniz20@gmail.com>
2023-04-08 13:08:29 -0300

    feat: add service ItemSaleService

---
+++ store-simulator/src/main/java/com/github/wesleyvlk/storesimulator/domain/model/service/ItemSaleService.java
@@ -0,0 +1,8 @@
+package com.github.wesleyvlk.storesimulator.domain.model.service;
+
+import com.github.wesleyvlk.storesimulator.domain.model.ItemSale;
+
+public interface ItemSaleService {
+
+    ItemSale addItemSale(ItemSale itemSale);
+}
