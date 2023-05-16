67a527234445db1f6904ae2f776b0e209d966394
wesleyvlk <wesleymuniz20@gmail.com>
2023-05-16 16:06:19 -0300

    refactor: Restructure store-simulator project
    From this commit, I am restructuring the store-simulator project to implement a subdomain architecture. This allows for better division of responsibilities, specifically for the endpoints: customers, products, and sales-carts endpoints along with your child endpoint, items.

--- store-simulator/src/main/java/com/github/wesleyvlk/storesimulator/domain/model/service/ItemSaleService.java
+++
@@ -1,8 +0,0 @@
-package com.github.wesleyvlk.storesimulator.domain.model.service;
-
-import com.github.wesleyvlk.storesimulator.domain.model.ItemSale;
-
-public interface ItemSaleService {
-
-    ItemSale addItemSale(ItemSale itemSale);
-}
--- store-simulator/src/main/java/com/github/wesleyvlk/storesimulator/domain/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.wesleyvlk.storesimulator.domain.repository;
-
-import com.github.wesleyvlk.storesimulator.domain.model.Customer;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CustomerRepository extends CrudRepository<Customer, Integer> {
-    Customer findByCpf(String cpf);
-
-}
